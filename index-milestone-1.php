511c3727a9c2880a500708b3b414643b98141e2d
AlexTaglia <alex.tagliabue@gmail.com>
2021-08-30 15:18:26 +0200

    Stampa a video dischi

---
+++ index-milestone-1.php
@@ -0,0 +1,46 @@
+<?php 
+/*
+Stampiamo i dischi solo con l’utilizzo di PHP, 
+che stampa direttamente i dischi in pagina: al caricamento della pagina 
+ci saranno tutti i dischi.
+*/
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/style.css">
+    <title>Php Ajax dischi</title>
+<body>
+
+<?php 
+    require_once __DIR__ . "/database/dischi.php";
+?>
+
+<div class="container">
+    <div class="row">
+
+        <?php 
+            foreach($dischi as $disco) {?>
+
+                <div class="card col-3" >
+                    <img src="<?php echo $disco['poster'] ?>" alt="">
+                    <div><?php echo $disco['title'] ?></div>
+                    <div><?php echo $disco['author'] ?></div>
+                    <div><?php echo $disco['year'] ?></div>
+                </div>
+
+            <?php
+            }
+        ?>
+
+    </div>
+
+</div>
+    
+</body>
+</html>
