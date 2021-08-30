4a660332abf6ab77c8c762a361bf76169b7ff3f9
AlexTaglia <alex.tagliabue@gmail.com>
2021-08-30 17:31:36 +0200

    Bonus 1 in progress

--- js/script.js
+++ js/script.js
@@ -4,16 +4,30 @@ new Vue(
     {
         el: '#app',
         data: {
-            dischi: []
+            dischi: [],
+            genreList: [],
+            selectedGenre:''
         },
         created() { 
             
             axios.get('http://localhost:8888/es/08.30%20php-ajax-dischi/php-ajax-dischi/backend/api.php')
-                .then((response) => {
+            .then((response) => {
                     this.dischi = response.data;
-            })   
+                    this.getGenresList();
+                })
+         
+        },
+        methods: {
+            getGenresList() {       
+                this.dischi.forEach((dischi) => {
+                    if(!this.genreList.includes(dischi.genre)){
+                        this.genreList.push(dischi.genre);
+                    }
+                }); 
+            },
 
         }
-
+        
     }
-);
+    );
+    
