865ad2b4d3697a9bb8813c623b67332b6c8da973
AlexTaglia <alex.tagliabue@gmail.com>
2021-08-30 16:46:10 +0200

    Stampa a video dischi con chiamata API

---
+++ js/script.js
@@ -0,0 +1,19 @@
+Vue.config.devtools = true;  
+
+new Vue(    
+    {
+        el: '#app',
+        data: {
+            dischi: []
+        },
+        created() { 
+            
+            axios.get('http://localhost:8888/es/08.30%20php-ajax-dischi/php-ajax-dischi/backend/api.php')
+                .then((response) => {
+                    this.dischi = response.data;
+            })   
+
+        }
+
+    }
+);
