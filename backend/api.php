968722c448953213e615fcbb2daa7c8212374abe
AlexTaglia <alex.tagliabue@gmail.com>
2021-08-31 12:59:11 +0200

    Risolto bug

--- backend/api.php
+++ backend/api.php
@@ -2,7 +2,11 @@
 
 require_once __DIR__ . "/../database/dischi.php";
 
-$genre = $_GET["genre"];
+if(!empty($_GET["genre"])){
+    $genre = $_GET["genre"];
+} else {
+    $genre = "";
+}
 
 if($genre){
     
